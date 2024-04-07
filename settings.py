fb64e1fd73d6d6c9ef0bee21d3b26e2ff72372dc
Mikhail Glukhov <michaelglukhoff@gmail.com>
2024-04-07 19:44:01 +0300

    working client and server sockets; add settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+connection_string = {
+    'host': "94.228.122.63",
+    'database': "screenshot_db",
+    'user': "gen_user",
+    'password': "+hx!Pa75=T9h@)"
+}
+
+screenshots_directory = r'C:\Users\Professional\PycharmProjects\screenshots\screenshots_dir'
+
+hot_key = 'ctrl+alt+k'
+
+server_file_path = r'C:\Users\Professional\PycharmProjects\screenshots\server_dir'
+server_host_port = ('localhost', 12345)
