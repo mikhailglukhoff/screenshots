73b309fcdf1fd3286be03eed89a085bd19bc08a6
Mikhail Glukhov <michaelglukhoff@gmail.com>
2024-04-14 15:37:59 +0300

    v 1.0 delete trash

--- main_qt5.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-
-from qt_classes import LoginWindow
-
-if __name__ == "__main__":
-    app = QApplication([])
-    login_window = LoginWindow()
-    login_window.show()
-    app.exec_()
